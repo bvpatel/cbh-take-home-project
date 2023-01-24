1202ce72be0ba6ab2bb89abc4b768eb6e82f6983
Bhavesh Patel <bhavesh.patel@fundingsocieties.com>
2023-01-24 07:28:59 +0530

    first commit

--- dpk.js
+++ dpk.js
@@ -1,24 +1,30 @@
 const crypto = require("crypto");
 
-
 exports.deterministicPartitionKey = (event) => {
+  const ALGORITHM = "sha3-512";
+  const DIGEST = "hex";
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let partitionKey = TRIVIAL_PARTITION_KEY;
+  let candidate;
 
-  if (event && event.partitionKey) {
-    partitionKey = event.partitionKey;
-  } else if (event) {
-    partitionKey = crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  if (event) {
+    if (event.partitionKey) {
+      candidate = event.partitionKey;
+    } else {
+      const data = JSON.stringify(event);
+      candidate = crypto.createHash(ALGORITHM).update(data).digest(DIGEST);
+    }
   }
 
-  if (typeof partitionKey !== "string") {
-    partitionKey = JSON.stringify(partitionKey);
+  if (candidate) {
+    if (typeof candidate !== "string") {
+      candidate = JSON.stringify(candidate);
+    }
+  } else {
+    candidate = TRIVIAL_PARTITION_KEY;
   }
-
-  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
-    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash(ALGORITHM).update(candidate).digest(DIGEST);
   }
-
-  return partitionKey;
+  return candidate;
 };
